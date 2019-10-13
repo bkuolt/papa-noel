ef2944b331221b7e9ccea14e716f70781a9c29f7
Bastian <b@k.com>
2019-10-13 06:50:07 +0200

    wip playground

--- src/playground/level.lua
+++ src/playground/level.lua
@@ -1,2 +1,26 @@
+-- Copyright 2019 Bastian Kuolt
 
--- TODO: think about how a lua level does look like
+function Level.pause()
+    -- TODO
+end
+
+function Level.resume()
+    -- TODO
+end
+
+function Level.addTile(sprite, position)
+    -- TODO
+end
+
+function Level.addItem(animation, position)
+    -- TODO
+end
+
+function Level.setCharacter(character, position)
+    -- TODO
+end
+
+function Level.setBackground(image)
+    -- TODO
+end 
+
